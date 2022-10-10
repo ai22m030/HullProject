fe2c432830b3c44d3239af6e486e28c27a44646d
Thomas Ville <ai22m045@technikum-wien.at>
2022-10-10 12:58:20 +0200

    Save performance

--- python_solution/divide_conquer.py
+++ python_solution/divide_conquer.py
@@ -1,18 +1,14 @@
+from turtle import color
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-import timeit
+from  timeit import Timer
 
 
 # This function simple generates a list of coordinates 
 def generate_k_xy(k):
     xy = np.random.rand(k,2)
     return xy
-
-#Input coordinationen
-xy = generate_k_xy(20)
-xy =xy[xy[:, 0].argsort()]
-
 
 #Navigate around the hull to find the top/bottom tanget
 def find_tangent(xy_left, xy_right):
@@ -36,25 +32,37 @@ def hull_merge(xy_left, xy_right, isinitial):
             if angle_between_vectors > 0:
                 placeholder_left.append(xy_left[rightmost_lefthull-2])
                 placeholder_left.append(xy_left[rightmost_lefthull-1])
-            else:
+            elif angle_between_vectors < 0:
                 placeholder_left.append(xy_left[rightmost_lefthull-1])
                 placeholder_left.append(xy_left[rightmost_lefthull-2])
+            #When the angle is 0, remove the middle point
+            else:
+                if np.linalg.norm(a) > np.linalg.norm(b):
+                    placeholder_left.append(xy_left[rightmost_lefthull-1])
+                else:
+                    placeholder_left.append(xy_left[rightmost_lefthull-2])
 
             xy_left = np.array(placeholder_left)
             rightmost_lefthull = 0
 
         placeholder_right = []
         if len(xy_right) == 3:
-            placeholder_right.append(xy_right[rightmost_lefthull])
-            a = (xy_right[rightmost_lefthull]-xy_right[rightmost_lefthull-1])
-            b = (xy_right[rightmost_lefthull]-xy_right[rightmost_lefthull-2])
+            placeholder_right.append(xy_right[leftmost_righthull])
+            a = (xy_right[leftmost_righthull]-xy_right[leftmost_righthull-1])
+            b = (xy_right[leftmost_righthull]-xy_right[leftmost_righthull-2])
             angle_between_vectors = np.cross(a,b)
             if angle_between_vectors > 0:
-                placeholder_right.append(xy_right[rightmost_lefthull-2])
-                placeholder_right.append(xy_right[rightmost_lefthull-1])
+                placeholder_right.append(xy_right[leftmost_righthull-2])
+                placeholder_right.append(xy_right[leftmost_righthull-1])
+            elif angle_between_vectors < 0:
+                placeholder_right.append(xy_right[leftmost_righthull-1])
+                placeholder_right.append(xy_right[leftmost_righthull-2])
+            #When the angle is 0, remove the middle point
             else:
-                placeholder_right.append(xy_right[rightmost_lefthull-1])
-                placeholder_right.append(xy_right[rightmost_lefthull-2])
+                if np.linalg.norm(a) > np.linalg.norm(b):
+                    placeholder_left.append(xy_left[rightmost_lefthull-1])
+                else:
+                    placeholder_left.append(xy_left[rightmost_lefthull-2])
 
             xy_right = np.array(placeholder_right)
             leftmost_righthull = 0
@@ -153,16 +161,30 @@ def hull_merge(xy_left, xy_right, isinitial):
     if q_top > q_bot:
         if p_top > p_bot:
             result = np.vstack((xy_left[p_top],xy_right[q_top:],xy_right[:q_bot+1],xy_left[p_bot:p_top]))
-        else:
+        elif p_top < p_bot:
             result = np.vstack((xy_left[:p_top+1],xy_right[q_top:],xy_right[:q_bot+1],xy_left[p_bot:]))
-    else:
+        #If p_top == p_bot (the same indice) this implies that the other points are surround by the hull and shouldn't be inlcuded
+        else:
+            result = np.vstack((xy_left[p_top],xy_right[q_top:],xy_right[:q_bot+1]))
+    elif q_top < q_bot:
         if p_top > p_bot:
             result = np.vstack((xy_left[p_top],xy_right[q_top:q_bot+1],xy_left[p_bot:p_top]))
-        else:
+        elif p_top < p_bot:
             result = np.vstack((xy_left[:p_top+1],xy_right[q_top:q_bot+1],xy_left[p_bot:])) 
-        
-    # plt.scatter(np.vstack((xy_left,xy_right))[:,0],np.vstack((xy_left,xy_right))[:,1])
-    # plt.show()
+        #If p_top == p_bot (the same indice) this implies that the other points are surround by the hull and shouldn't be inlcuded
+        else:
+            result = np.vstack((xy_left[p_top],xy_right[q_top:q_bot+1]))
+    #If q_top == q_bot (the same indice) this implies that the other points are surround by the hull and shouldn't be inlcuded
+    else:
+        if p_top > p_bot:
+            result = np.vstack((xy_left[p_top],xy_right[q_top],xy_left[p_bot:p_top]))
+        elif p_top < p_bot:
+            result = np.vstack((xy_left[:p_top+1],xy_right[q_top],xy_left[p_bot:])) 
+        #If p_top == p_bot (the same indice) this implies that the other points are surround by the hull and shouldn't be inlcuded
+        #Note: This case will never happen because this implies that both the left and the right hull surround eachother because this is just a line
+        else:
+            result = np.vstack((xy_left[p_top],xy_right[q_top]))
+
     return result
 
 
@@ -181,17 +203,36 @@ def convex_hull(xy,recursion_level):
     merge_hulls[str(recursion_level)].append(hull_merge_it)
     return hull_merge_it, False
 
-print(convex_hull(xy,0))
-print(merge_hulls)
-plt.figure()
-plt.scatter(xy[:,0],xy[:,1])
+#Input coordinationen
+k = 11
+# for k in range(101,1001):
+xy = generate_k_xy(k)
+xy =xy[xy[:, 0].argsort()]
 
+t = Timer(lambda: convex_hull(xy,0))
+#Save the timerequired to the file "o_notationdata.txt"
+mat = np.matrix([[1,2],[4,5],[7,8]])
+matrix = np.loadtxt('./python_solution/o_notation_data.txt')
+#Find the current time value for a specific k (note that k is stored in the first column)
+k_index = np.where(matrix[:,0] == k)[0]
+if k_index.size == 0:
+    matrix = np.vstack((matrix,[k,t.timeit(number=1)]))
+else:
+    #Get average value between current and previous
+    matrix[k_index[0]][1] = (matrix[k_index[0]][1] + t.timeit(number=1))/2
+with open('./python_solution/o_notation_data.txt', 'wb') as f:
+    np.savetxt(f, matrix, delimiter=' ')
+
+
+# Plot everything
+plt.figure()
+plt.scatter(xy[:,0],xy[:,1], color='grey')
 
 for keys in merge_hulls:
     fig ,ax = plt.subplots(1,1)
     for hulls in merge_hulls[keys]:
         hulls_temp = np.vstack((hulls,hulls[0]))
-        plt.scatter(xy[:,0],xy[:,1])
+        plt.scatter(xy[:,0],xy[:,1],color ='grey')
         ax.plot(hulls_temp[:,0],hulls_temp[:,1])
 
 
